4e881db7bece4732c53e7ea8c940fcbfbe174e2c
Akshay <gurao.a@husky.neu.edu>
2018-03-27 07:00:36 -0400

    Added email functionality

--- lambda-test.py
+++ lambda-test.py
@@ -2,6 +2,7 @@ def lambda_handler(event, context):
     # TODO implement
     import uuid
     import boto3
+    from botocore.exceptions import ClientError
     myuuid = uuid.uuid4()
     print('Hello from lambda')
     print("My uuid is",myuuid)
@@ -15,7 +16,7 @@ def lambda_handler(event, context):
 
     response = table.put_item(
     Item={
-            'email': 'vighnesh@gmail.com',
+            'id': event["email"],
             'uuid': str(myuuid)
         }
     )
@@ -23,5 +24,68 @@ def lambda_handler(event, context):
     print("PutItem succeeded:")
     print(response)
     #context.getLogger().log("1",event.getRecords().size())
+
+
+
+
+
+    SENDER = "Sender Name <gurao.a@husky.neu.edu>"
+    RECIPIENT =  event["email"]
+    AWS_REGION = "us-east-1"
+    SUBJECT = "Amazon SES Test (SDK for Python)"
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                 "This email was sent with Amazon SES using the "
+                 "AWS SDK for Python (Boto)."
+                )
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+      <h1>Amazon SES Test (SDK for Python)</h1>
+      <p>This email was sent with
+        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+        <a href='https://aws.amazon.com/sdk-for-python/'>
+          AWS SDK for Python (Boto)</a>.</p>
+    </body>
+    </html>
+                """
+    CHARSET = "UTF-8"
+
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+
+        )
+
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+    print(response['ResponseMetadata']['RequestId'])
+
+
     return 'Hello from Lambda'
     
